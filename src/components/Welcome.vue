753cb7c27291c155dc4b7ae3a289b68c24502d7a
Loek Sangers <9132841+LoekSangers@users.noreply.github.com>
2021-12-25 19:22:29 +0000

    Added links to other profiles

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="welcome">
       <h1>Loek Sangers</h1>
-      <p>Welcome on my personal website. It is currently still under construction so this is all you are getting for now :).</p>
+      <p>Welcome on my personal website. It is currently still under construction - and it might be in this state for a while -so this is all you are getting for now :).</p>
+      <p>For more information about me feel free to visit my <a href="https://www.linkedin.com/in/loeksangers/">LinkedIn</a> or <a href="https://github.com/LoekSangers">GitHub</a> profile</p>
   </div>
 </template>
 
