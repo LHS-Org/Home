e3be294d33821fff0df353bd8f4d3d82f3d25271
Loek Sangers <9132841+LoekSangers@users.noreply.github.com>
2022-01-13 19:58:53 +0000

    Update landing page

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="welcome">
       <h1>Loek Sangers</h1>
-      <p>Welcome on my personal website. It is currently still under construction - and it might be in this state for a while - so this is all you are getting for now :).</p>
-      <p>For more information about me feel free to visit my <a href="https://www.linkedin.com/in/loeksangers/">LinkedIn</a> or <a href="https://github.com/LoekSangers">GitHub</a> profile</p>
+      <p>Feel free to contact me on <a href="https://www.linkedin.com/in/loeksangers/">LinkedIn</a> or see what I have been up to lately on <a href="https://github.com/LoekSangers">GitHub</a>.</p>
+      <p>If you were expecting an introduction, those places are also where you need to be ;).</p>
   </div>
 </template>
 
